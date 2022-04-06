33a5b772bb0a83a77a2e92f76ddb63c81dfa49a1
kibates7 <bates.kiara@gmail.com>
2022-04-05 20:53:56 -0500

    Class work April 5th

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let selectedBox = ''
-let blocks = document.getElementsByClassName("block") 
+let blocks = document.getElementsByClassName("block")
 let char = ''
+let result = document.getElementById("result")
+let board = document.getElementById("board")
 
 const gameState = {
     players: ['x', 'o'], //current player is whomever in 0 position and swap after each play
@@ -17,30 +19,6 @@ function swapTurns (){
     gameState.players[0] = gameState.players[1]
     gameState.players[1] = temp
 }
-
-
-
-//on click populate the block
-//if block has not been clicked, populate with correct x or o
-document.querySelectorAll('.block').forEach(block => {
-    block.addEventListener('click', event => {
-        char = gameState.players[0]
-        if(block.innerHTML === ""){
-            block.insertAdjacentHTML("afterbegin", char);
-            selectedBox = block.getAttribute('id')
-            swapTurns()
-            addBoard(selectedBox, char)
-            //console.log(gameState.board)
-        }
-        
-        
-        //addBoard(char)
-        //console.log()
-        
-       
-    })
-})
-
 
 
 //check if this click completes the 3 in a row, if true end game --> disable board
@@ -76,17 +54,85 @@ function addBoard(selectedNum, char){
     else if(num === 8){
      col = 2
     }
+    }
 
     if(gameState.board[row][col] === null){
         gameState.board[row][col] = char
     }
+    return row
+
+    // console.log("addBoard function just ran")
+    // console.log(num, row, col)
+}
+
+
+//check if play was a winning one
+//compare small arrays from gamestate.board
+function getRow(row){
+    let sameRow = true
     
-    console.log(gameState.board)
+        let across = gameState.board[row]
+        console.log("row is ", across)
+        for(let j = 0; j < across.length; j++){
+            for(k = j + 1; k < across.length; k++){
+                if(across[j] !== across[k]){
+                    sameRow = false
+                }
+            }
+        }
+    
+    
+    return sameRow
+
+    
 }
+
+//check if column is a winner
+function getCol(){
+    // let col = 0
+    let board = gameState.board
+    let sameCol = true
+    for(let i = 0; i < board.length; i++){
+        let col = board.map(b => b[i])
+        console.log(col)
+        for(let j = 0; j < col.length; j++){
+            for(k = j + 1; k < col.length; k++){
+                if(col[j] !== col[k]){
+                    sameCol = false;
+                }
+            }
+        }
+    }
+    console.log(sameCol)
+    
 }
 
 
-
+//on click populate the block
+//if block has not been clicked, populate with correct x or o
+for (const blck of blocks){
+    blck.addEventListener('click', (event) =>{
+        char = gameState.players[0]
+        
+            if(blck.innerHTML === ""){
+                blck.insertAdjacentHTML("afterbegin", char);
+                selectedBox = blck.getAttribute('id')
+                
+                swapTurns()
+                
+                // if user wins across row, disable the board
+                if(getRow((addBoard(selectedBox, char)))){
+                    result.innerHTML = "Congratulations!"
+                    // board.setAttribute("style", "pointer-events:none")
+                    // console.log(typeof board)
+                }
+                
+            }
+            //getCol()
+            
+            
+    })
+    }
 
 
 //start over
